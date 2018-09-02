cc32bdaa8c7dee3185fd46eaff3aa0a796e65eff
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-09-03 07:50:41 +1000

    Add a message and change the app guid

--- src/KeepKey.Net.XamarinFormsSample/KeepKey.Net.XamarinFormsSample.UWP/MainPage.xaml.cs
+++ src/KeepKey.Net.XamarinFormsSample/KeepKey.Net.XamarinFormsSample.UWP/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Hid.Net;
 using Hid.Net.UWP;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
 using app = KeepKey.Net.XamarinFormsSample.App;
 
 namespace KeepKey.Net.XamarinFormsSample.UWP
@@ -18,6 +19,9 @@ namespace KeepKey.Net.XamarinFormsSample.UWP
             keepKeyHidDevice.Connected += KeepKeyHidDevice_Connected;
             poller = new UWPHidDevicePoller(KeepKeyManager.ProductId, KeepKeyManager.VendorId, keepKeyHidDevice);
             LoadApplication(new app(keepKeyHidDevice));
+
+            new MessageDialog("Unfortunately, KeepKey's Vendor Id is not accepted by the UWP runtime at the permissions level currently. Please check again soon and see https://github.com/MelbourneDeveloper/KeepKey.Net/issues/2").ShowAsync();
+
         }
 
         private void KeepKeyHidDevice_Connected(object sender, System.EventArgs e)
