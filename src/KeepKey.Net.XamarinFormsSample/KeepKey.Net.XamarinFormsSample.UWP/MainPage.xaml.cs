19efe775b48aa810cfd771fa5db319185c070406
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-11-24 16:13:03 +1100

    Fix the device capabiltiy

--- src/KeepKey.Net.XamarinFormsSample/KeepKey.Net.XamarinFormsSample.UWP/MainPage.xaml.cs
+++ src/KeepKey.Net.XamarinFormsSample/KeepKey.Net.XamarinFormsSample.UWP/MainPage.xaml.cs
@@ -19,9 +19,6 @@ namespace KeepKey.Net.XamarinFormsSample.UWP
             keepKeyHidDevice.Connected += KeepKeyHidDevice_Connected;
             poller = new UWPHidDevicePoller(KeepKeyManager.ProductId, KeepKeyManager.VendorId, keepKeyHidDevice);
             LoadApplication(new app(keepKeyHidDevice));
-
-            new MessageDialog("Unfortunately, KeepKey's Vendor Id is not accepted by the UWP runtime at the permissions level currently. Please check again soon and see https://github.com/MelbourneDeveloper/KeepKey.Net/issues/2").ShowAsync();
-
         }
 
         private void KeepKeyHidDevice_Connected(object sender, System.EventArgs e)
