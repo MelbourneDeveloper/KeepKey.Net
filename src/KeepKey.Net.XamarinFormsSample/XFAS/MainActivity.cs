b36646647b62654f916c7d21e6c7bdbaeeaae717
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-01-26 19:23:46 +1100

    Android app compiles

--- src/KeepKey.Net.XamarinFormsSample/XFAS/MainActivity.cs
+++ src/KeepKey.Net.XamarinFormsSample/XFAS/MainActivity.cs
@@ -4,85 +4,38 @@ using Android.Content;
 using Android.Content.PM;
 using Android.Hardware.Usb;
 using Android.OS;
-using Usb.Net.Android;
+using Android.Widget;
+using Device.Net;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
 namespace KeepKey.Net.XamarinFormsSample.Droid
 {
-    public class DebugTracer : Device.Net.ITracer
-    {
-        public string WriteSuffix { get; set; } = "KeepKeyIOWrite";
-        public string ReadSuffix { get; set; } = "KeepKeyIORead";
-
-        public void Trace(bool isWrite, byte[] data)
-        {
-            System.Diagnostics.Debug.WriteLine($"({string.Join(",", data)}) - {(isWrite ? WriteSuffix : ReadSuffix)} ({data.Length})");
-        }
-    }
-
     [IntentFilter(new[] { UsbManager.ActionUsbDeviceAttached })]
     [Activity(Label = "KeepKey.Net.XamarinFormsSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : FormsAppCompatActivity
     {
-        #region Fields
-        private AndroidUsbDevice _KeepKeyHidDevice;
-        private UsbDeviceAttachedReceiver _KeepKeyUsbDeviceAttachedReceiver;
-        private UsbDeviceDetachedReceiver _KeepKeyUsbDeviceDetachedReceiver;
-        private readonly object _ReceiverLock = new object();
-        #endregion
-
         protected override void OnCreate(Bundle savedInstanceState)
-        {
-            _KeepKeyHidDevice = new AndroidUsbDevice(GetSystemService(UsbService) as UsbManager, ApplicationContext, 3000, 64, KeepKeyManager.VendorId, KeepKeyManager.ProductId);
-
-            _KeepKeyHidDevice.Tracer = new DebugTracer();
-
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
-            base.OnCreate(savedInstanceState);
-            Forms.Init(this, savedInstanceState);
-            RegisterReceiver();
-            LoadApplication(new App(_KeepKeyHidDevice));
-        }
-
-        protected override void OnResume()
-        {
-            base.OnResume();
-            RegisterReceiver();
-        }
-
-        private void RegisterReceiver()
         {
             try
             {
-                lock (_ReceiverLock)
-                {
-                    if (_KeepKeyUsbDeviceAttachedReceiver != null)
-                    {
-                        UnregisterReceiver(_KeepKeyUsbDeviceAttachedReceiver);
-                        _KeepKeyUsbDeviceAttachedReceiver.Dispose();
-                    }
+                var usbManager = GetSystemService(UsbService) as UsbManager;
+                if (usbManager == null) throw new Exception("UsbManager is null");
 
-                    _KeepKeyUsbDeviceAttachedReceiver = new UsbDeviceAttachedReceiver(_KeepKeyHidDevice);
-                    RegisterReceiver(_KeepKeyUsbDeviceAttachedReceiver, new IntentFilter(UsbManager.ActionUsbDeviceAttached));
+                //Register the factory for creating Usb devices. This only needs to be done once.
+                AndroidUsbDeviceFactory.Register(usbManager, base.ApplicationContext);
 
+                TabLayoutResource = Resource.Layout.Tabbar;
+                ToolbarResource = Resource.Layout.Toolbar;
 
-                    if (_KeepKeyUsbDeviceDetachedReceiver != null)
-                    {
-                        UnregisterReceiver(_KeepKeyUsbDeviceDetachedReceiver);
-                        _KeepKeyUsbDeviceDetachedReceiver.Dispose();
-                    }
-
-
-                    _KeepKeyUsbDeviceDetachedReceiver = new UsbDeviceDetachedReceiver(_KeepKeyHidDevice);
-                    RegisterReceiver(_KeepKeyUsbDeviceDetachedReceiver, new IntentFilter(UsbManager.ActionUsbDeviceDetached));
-                }
+                base.OnCreate(savedInstanceState);
+                Forms.Init(this, savedInstanceState);
+                LoadApplication(new App());
             }
-            catch
+            catch(Exception ex)
             {
-
+                Toast.MakeText(ApplicationContext, ex.ToString(), ToastLength.Long).Show();
             }
         }
     }
